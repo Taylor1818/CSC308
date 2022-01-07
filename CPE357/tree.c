828f1cb5c97dcd9ab86b56e825be231eaf5c1686
Taylor1818 <tbcmorgan@gmail.com>
2022-01-07 14:50:38 -0800

    moved to 357

--- CPE357/tree.c
+++ CPE357/tree.c
@@ -5,24 +5,27 @@ void buildTree(int *height);
 
 int main(int arg, char *arg1[]){
 
-    int height; 
+    
+    int height = 12; 
     FILE *file = NULL;
 
     if(arg < 1){
         printf( "Enter the Height of the Tree: " );
         scanf( "%d", &height );
+        
+        //file = fopen( arg1[2], "w" );
     }
-    else if(arg > 1){
-        file = fopen( arg1[2], "w" );
+    
+    else if(arg1 > 0){
+        //height = (arg1[1]);
+        if( height > 0 && height < 15){
+            //height = (int)arg1[1];
+        }
+        else{
+            //exit
+        }
     }
-    else{
-        height = atoi( arg1[1] );
-    }
-
-
-    if(height < 0 || height > 15){
-        // Exit restart
-    }
+        
 
     buildTree(&height);
 
@@ -32,8 +35,8 @@ int main(int arg, char *arg1[]){
 void buildTree(int *height){
     for(int i = 1; i <= *height; i++){
         if( *height - i >= 3){
-            *height -=  3;
-            for(int j = 0; j < *height - i; j++){
+            //*height -=  3;
+            for(int j = 0; j < *height - i - 3; j++){
                 putchar(' ');
             }
             for(int j = 1; j < i -2; j++){
