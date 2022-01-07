fb5abff62fdb6e61a34b0e4635de4dbd18f90546
Taylor1818 <tbcmorgan@gmail.com>
2022-01-07 15:31:24 -0800

    A second commit of my react app

--- CPE308/react-assignment1/src/MyApp.js
+++ CPE308/react-assignment1/src/MyApp.js
@@ -1,14 +1,10 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
+
 
 function MyApp() {
-   const [characters, setCharacters] = useState([  
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-        // the rest of the data
-      },
-    ]);
+    const [characters, setCharacters] = useState([]);
 
    function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
@@ -16,13 +12,18 @@ function MyApp() {
       });
       setCharacters(updated);
    }
-   
+
    return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   );
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+  
 }
 
 export default MyApp;
