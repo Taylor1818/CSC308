7865ba9e4351c722d82055d3c6a23b6b87df3bdb
Taylor1818 <tbcmorgan@gmail.com>
2022-01-07 18:17:48 -0800

    fixed the tree

--- CPE357/tree.c
+++ CPE357/tree.c
@@ -1,50 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void buildTree(int *height);
+void buildTree(int height);
 
 int main(int arg, char *arg1[]){
 
     
-    int height = 12; 
+    int height; 
     FILE *file = NULL;
 
-    if(arg < 1){
+    if(arg < 2 ){
         printf( "Enter the Height of the Tree: " );
         scanf( "%d", &height );
         
         //file = fopen( arg1[2], "w" );
     }
-    
-    else if(arg1 > 0){
-        //height = (arg1[1]);
-        if( height > 0 && height < 15){
-            //height = (int)arg1[1];
-        }
-        else{
-            //exit
-        }
+    else{
+        height = atoi(arg1[1]);
     }
-        
 
-    buildTree(&height);
-
-    return 0;
+    if( height >= 0 && height <= 15){
+        buildTree(height);
+        return 0;
+    }
+    
+    return -1;
+    
 }
 
-void buildTree(int *height){
-    for(int i = 1; i <= *height; i++){
-        if( *height - i >= 3){
-            //*height -=  3;
-            for(int j = 0; j < *height - i - 3; j++){
+void buildTree(int height){
+    for(int i = 1; i <= height; i++){
+        if( height - i >= 3){
+            for(int j = 0; j < height - i - 3; j++){
                 putchar(' ');
             }
-            for(int j = 1; j < i -2; j++){
+            for(int j = 0; j < 2*i - 1; j++){
                 putchar('*');
             }
         }
         else{
-            for(int i = 0; i < *height - 4; i++){
+            for(int i = 0; i < height - 4; i++){
                 putchar(' ');
             }
             putchar('*');
