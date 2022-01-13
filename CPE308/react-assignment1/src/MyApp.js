634e3643f3a80ce74e8b6d16d6923bd9ea9d274e
Taylor1818 <tbcmorgan@gmail.com>
2022-01-13 15:24:22 -0800

    Frontend

--- CPE308/react-assignment1/src/MyApp.js
+++ CPE308/react-assignment1/src/MyApp.js
@@ -1,10 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
+
 
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+    }, [] );
 
    function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
@@ -20,9 +29,40 @@ function MyApp() {
     </div>
   );
 
+
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+ }
+
+ async function makePostCall(person){
+  try {
+     const response = await axios.post('http://localhost:5000/users', person);
+     return response;
+  }
+  catch (error) {
+     console.log(error);
+     return false;
+  }
+}
+
+function updateList(person) { 
+  makePostCall(person).then( result => {
+  if (result && result.status === 201)
+     setCharacters([...characters, person] );
+  });
+}
   
 }
 
